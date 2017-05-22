c4a6978ce83ad1a5566be9bd2fc99d3fa1efcca6
chen <chen@qq.com>
2017-05-22 18:14:59 +0800

    1.加载配置项

---
+++ src/main/java/org/smart4j/framework/util/StringUtil.java
@@ -0,0 +1,39 @@
+package org.smart4j.framework.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * 字符串工具类
+ */
+public final class StringUtil {
+
+    /**
+     * 判断字符串是否为空
+     *
+     * @param str
+     *              目标字符串
+     * @return
+     */
+    public static boolean isEmpty(String str) {
+
+        if (str != null) {
+            str = str.trim();
+        }
+        return StringUtils.isEmpty(str);
+
+    }
+
+    /**
+     * 判断字符串是否非空
+     *
+     * @param str
+     *              目标字符串
+     * @return
+     */
+    public static boolean isNotEmpty(String str) {
+
+        return !isEmpty(str);
+
+    }
+
+}
