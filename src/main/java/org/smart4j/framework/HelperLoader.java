67fd49a7be955e580cb2dfd622d8847de6a6c296
chen <chen@qq.com>
2017-05-23 17:57:34 +0800

    1.设置JVM查找并加载指定的类时进行初始化

--- src/main/java/org/smart4j/framework/HelperLoader.java
+++ src/main/java/org/smart4j/framework/HelperLoader.java
@@ -20,7 +20,7 @@ public final class HelperLoader {
                 ControllerHelper.class
         };
         for (Class<?> cls: classList) {
-            ClassUtil.loadClass(cls.getName(), false);
+            ClassUtil.loadClass(cls.getName(), true);
         }
 
     }
